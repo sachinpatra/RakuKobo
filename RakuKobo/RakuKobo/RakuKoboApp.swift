4e448893a08ef728490b4cc1e0685c99b8613652
Sachin <skpatra@Kirusas-MacBook-Pro.local>
2020-12-11 18:11:31 +0530

    Initial Commit

---
+++ RakuKobo/RakuKobo/RakuKoboApp.swift
@@ -0,0 +1,17 @@
+//
+//  RakuKoboApp.swift
+//  RakuKobo
+//
+//  Created by Sachin Kumar Patra on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RakuKoboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
